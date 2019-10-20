3c5aeed1824e4309b583ade5ae18c39dd18d3f0c
Alberto Diaz lopez <adiaz-lo@c1r17s5.42madrid.com>
2019-10-20 10:59:41 +0200

    Termination of C02 (retry 1) ex10 exercise.

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adiaz-lo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/17 11:37:01 by adiaz-lo          #+#    #+#             */
+/*   Updated: 2019/10/20 10:48:58 by adiaz-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int				ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (ft_strlen(src));
+	}
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
